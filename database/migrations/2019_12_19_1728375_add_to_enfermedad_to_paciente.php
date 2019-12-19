af12c983acaf2bfb6344ad532ca153f4660ee9bd
pilimima <pilarmima97@gmail.com>
2019-12-19 16:41:59 +0100

    Añadido el atributo localizacion a la clase citas Añadido fecha final a la cita Añadido enfermedad a paciente y a cita cascade

--- database/migrations/2019_12_19_1728375_add_to_enfermedad_to_paciente.php
+++ database/migrations/2019_12_19_1728375_add_to_enfermedad_to_paciente.php
@@ -14,8 +14,8 @@ class AddToEnfermedadToPaciente extends Migration
     public function up()
     {
         Schema::table('pacientes', function (Blueprint $table) {
-            $table->unsignedInteger('enfermedad_id');
-            $table->foreign('enfermedad_id')->references('id')->on('enfermedads');
+            $table->unsignedInteger('enfermedad_id')->nullable();
+            $table->foreign('enfermedad_id')->references('id')->on('enfermedads')->onDelete('cascade');
         });
     }
 
